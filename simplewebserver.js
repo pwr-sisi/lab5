0bc3086d6189b80729b5daa4667fc78bc8feb990
Wojciech Thomas <wojciech.thomas@pwr.edu.pl>
2019-05-10 13:44:41 +0200

    Removed empty line simplewebserver

--- simplewebserver.js
+++ simplewebserver.js
@@ -1,11 +1,13 @@
+// Załadowanie modułu http
 var http = require("http");
 
+// Utworzenie serwera - definicja funkcji odpowiadającej na zapytania HTTP
 var server = http.createServer(function (req, res) {
    res.writeHead(200, {'Content-Type': 'text/plain'});
-   
    res.end('Hello World\n');
 })
 
+// Uruchomienie serwera na porcie 3000
 server.listen(3000);
 
 console.log('Server running at http://127.0.0.1:3000/');
