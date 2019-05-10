f674612ee5d9e9b2b55ae49dccf165b1b95891f4
Wojciech Thomas <wojciech.thomas@pwr.edu.pl>
2019-05-10 13:25:40 +0200

    Added server object to simplewebserver.js

--- simplewebserver.js
+++ simplewebserver.js
@@ -1,12 +1,11 @@
-// Import modułu http
 var http = require("http");
 
-http.createServer(function (req, res) {
-   // Wysłanie nagłówka HTTP
+var server = http.createServer(function (req, res) {
    res.writeHead(200, {'Content-Type': 'text/plain'});
    
-   // Wysłanie danych (tekstu)
    res.end('Hello World\n');
-}).listen(3000);
+})
+
+server.listen(3000);
 
 console.log('Server running at http://127.0.0.1:3000/');
